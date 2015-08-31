ecb6188050797fd2c692a24bfe768b008153b080
vinay mattu <vmattu9@gmail.com>
2015-08-31 09:17:23 +0530

    commitig

--- demo1/src/mainpack/MainApp.java
+++ demo1/src/mainpack/MainApp.java
@@ -1,18 +1,12 @@
-package mainpack; 
-
-import Math.Airthematic;
-
-
-
+package MainPack;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          Airthematic  a;
-          a =new Airthematic();
-          
-          a.sum(4,3);
+		int a;
+		System.out.println();
+
 	}
 
 }
